23e2878aab4bede485d302425e4e16348bb920e2
fernandoallisson <fernando.alisson@ufpe.br>
2023-10-06 14:56:07 -0300

    Criando o CRUD

--- src/app.js
+++ src/app.js
@@ -2,12 +2,53 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => { // http://localhost:3003/
-  res.status(200).json({ message: 'Hello World!' });
+const teams = [
+  {
+    id: 1,
+    name: 'São Paulo Futebol Clube',
+    initials: 'SPF',
+  },
+  {
+    id: 2,
+    name: 'Clube Atlético Mineiro',
+    initials: 'CAM',
+  },
+];
+
+app.use(express.json());
+
+app.get('/teams', (req, res) => {
+  res.status(200).json({ teams });
 });
 
-app.get('/Ola', (req, res) => { // http://localhost:3003/Ola
-  res.send('<h1>Olá Mundo!</h1>');
+app.post('/teams', (req, res) => {
+  const newTeam = { ...req.body };
+  teams.push(newTeam);
+  res.status(201).json({ team: newTeam });
+});
+
+app.put('/teams/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, initials } = req.body;
+
+  const update = teams.find((team) => team.id === Number(id));
+
+  if (!update) {
+    return res.status(404).json({ message: 'Team Not Found' });
+  }
+  update.name = name;
+  update.initials = initials;
+  res.status(200).json({ update });
+});
+
+app.delete('/teams/:id', (req, res) => {
+  const { id } = req.params;
+  
+  const delet = teams.findIndex((team) => team.id === Number(id));
+
+  teams.splice(delet, 1);
+
+  res.status(200).end();
 });
 
 module.exports = app;
